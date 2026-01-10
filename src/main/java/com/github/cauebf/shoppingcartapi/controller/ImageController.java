2aa8d7ff15a4ee92a5b0d757fb0c76ceba60e737
Cauê Favarim <cauebrolesef@gmail.com>
2026-01-10 11:56:09 -0300

    feat: ImageController

---
+++ src/main/java/com/github/cauebf/shoppingcartapi/controller/ImageController.java
@@ -0,0 +1,90 @@
+package com.github.cauebf.shoppingcartapi.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.github.cauebf.shoppingcartapi.dto.ImageDto;
+import com.github.cauebf.shoppingcartapi.exceptions.ResourceNotFoundException;
+import com.github.cauebf.shoppingcartapi.model.Image;
+import com.github.cauebf.shoppingcartapi.response.ApiResponse;
+import com.github.cauebf.shoppingcartapi.service.image.IImageService;
+
+@RestController
+@RequestMapping("${api.prefix}/images")
+public class ImageController {
+    private final IImageService imageService;
+
+    public ImageController(IImageService imageService) {
+        // constructor dependency injection
+        this.imageService = imageService;
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        // convert the image BLOB into bytes so it can be sent in the response
+        ByteArrayResource resouce = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        // return the image in the response
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(image.getFileType())) // set the correct content type (image/png, image/jpeg)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"") // tell the browser to download the file
+                .body(resouce); // set the body of the response to the image bytes
+    }
+
+    @PostMapping("/upload") 
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
+        try {
+            List<ImageDto> imageDtos = imageService.saveImages(files, productId); 
+            return ResponseEntity.ok(new ApiResponse("Upload success!", imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed!", e.getMessage()));
+        }
+    }
+
+    @PutMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+
+            if (image != null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("Image updated successfully!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Update failed!", HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+
+            if (image != null) {
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Image deleted successfully!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed!", HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+}
