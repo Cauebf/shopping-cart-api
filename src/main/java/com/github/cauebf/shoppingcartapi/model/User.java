cbf0706f795fe71cb1f9c4254ee45406fcfdf197
Cauê Favarim <cauebrolesef@gmail.com>
2026-01-15 21:34:07 -0300

    feat: add Role model

--- src/main/java/com/github/cauebf/shoppingcartapi/model/User.java
+++ src/main/java/com/github/cauebf/shoppingcartapi/model/User.java
@@ -1,12 +1,20 @@
 package com.github.cauebf.shoppingcartapi.model;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+
+import org.hibernate.annotations.NaturalId;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -27,6 +35,7 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @NaturalId // unique
     private String email;
     private String password;
 
@@ -35,4 +44,20 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true) // if delete user, delete orders
     private List<Order> orders;
+
+    @ManyToMany(
+        fetch = FetchType.EAGER, // load roles together with user
+        cascade = { 
+            CascadeType.PERSIST, // saves new roles when saving user
+            CascadeType.MERGE, // updates roles when updating user
+            CascadeType.DETACH, // detaches roles when the user is detached
+            CascadeType.REFRESH // refreshes roles when the user is refreshed
+            // CascadeType.REMOVE // deletes roles when the user is deleted
+        }
+    ) 
+    @JoinTable( // intermediate table between user and role
+        name = "user_roles", // table name
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), // foreign key of user
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")) // foreign key of role
+    private Collection<Role> roles = new HashSet<>();
 }
